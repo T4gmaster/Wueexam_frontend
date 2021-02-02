153650f18fbc99f39138eb01413baaa4808409be
Philip Gottwald <philip.gottwald@tech11.com>
2021-02-02 03:21:26 +0100

    add axios function to call solver routings

--- src/pages/Solver.vue
+++ src/pages/Solver.vue
@@ -1,12 +1,36 @@
 <template>
-<div>
-  <button>Start Solver</button>
-  <button>Stop Solver</button>
-</div>
-
+  <div>
+    <button v-on:click="startSolver()">Solver starten</button>
+    <button v-on:click="stopSolver()">Solver beenden</button>
+  </div>
 </template>
+
 <script>
-export default {};
+import axios from 'axios';
+import Button from '../components/Button.vue';
+export default {
+  components: { Button },
+  methods: {
+      startSolver(){
+          axios.post('localhost:5000/startsolver')
+          .then(function (response) {
+              console.log(response);
+          })
+          .catch(function() {
+              console.log('Solver konnte nicht gestartet werden');
+          });
+      },
+      stopSolver(){
+          axios.post('localhost:5000/stopsolver')
+          .then(function (response) {
+              console.log(response);
+          })
+          .catch(function () {
+              console.log('Solver konnte nicht gestopt werden');
+          });
+      }
+  }
+};
 </script>
 <style>
 </style>
