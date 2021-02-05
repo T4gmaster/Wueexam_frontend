8ca17ac91966c5f0a47f6248389ce251abdada2d
Philip Gottwald <philip.gottwald@tech11.com>
2021-02-05 03:31:36 +0100

    exam input param

--- src/pages/Prüfungsparameter.vue
+++ src/pages/Prüfungsparameter.vue
@@ -1,59 +1,121 @@
 <template>
   <div>
-    <h2>Startdatum</h2>
-    <b-calendar v-model="startDate" @context="logStartDate" locale="de"></b-calendar>
-    <h2>Enddatum</h2>
-    <b-calendar v-model="endDate" @context="logEndDate" locale="de"></b-calendar>
-    <h3>Prüfungszeitraum: {{ examPeriod }} Tage</h3>
-    <button @click="getDates">einzelne Tage (in Console) anzeigen</button>
-    <!--<li :v-for="index in dateArray"></li> !-->
-
+    <b-container fluid>
+      <b-row>
+        <b-col>
+          <h2>Startdatum</h2>
+          <b-calendar
+            v-model="startDate"
+            @context="logStartDate"
+            locale="de"
+          ></b-calendar>
+        </b-col>
+        <b-col>
+          <h2>Enddatum</h2>
+          <b-calendar
+            v-model="endDate"
+            @context="logEndDate"
+            locale="de"
+          ></b-calendar>
+        </b-col>
+        <b-col>
+          <h3>Prüfungszeitraum: {{ examPeriod }} Tage</h3>
+          <button @click="getDates">einzelne Tage (in Console) anzeigen</button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <h2>Prüfungsübersicht</h2>
+          <b-table striped hover :items="examOverview">
+            <template #cell(show_details)="row">
+              <b-button size="sm" @click="row.toggleDetails" class="mr-2"
+                >Fixdatum festlegen</b-button
+              >
+            </template>
+            <template #row-details="row">
+              <b-card>
+                <b-row class="mb-2">
+                  <h2>test</h2>
+                </b-row>
+                <b-button size="sm" @click="row.toggleDetails"
+                  >Hide Details</b-button
+                >
+              </b-card>
+            </template>
+          </b-table>
+        </b-col>
+        <b-col>
+          <h2>Prüfungstage</h2>
+          <!-- fix: Prüfungstage anzeigen
+          <b-table striped hover :items="period"></b-table>!-->
+          <ul>
+            <li v-for="item in period" :key="item">
+              {{ period.Prüfungstage }}
+            </li>
+          </ul>
+          <p>{{ period }}</p>
+          <button @click="testtest">Prüfungen logen</button>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
+import testpruefungen from "@/assets/testpruefungen.json";
+
 export default {
   data() {
     return {
-      startDate: '',
-      endDate: ''
-    }
+      startDate: "",
+      endDate: "",
+      examOverview: testpruefungen,
+      period: "",
+    };
   },
   computed: {
-    examPeriod () {
-      let start = moment(this.startDate)
-      let end = moment(this.endDate)
-      let days = end.diff(start, "days")
-      return days
-    }
+    examPeriod() {
+      let start = moment(this.startDate);
+      let end = moment(this.endDate);
+      let days = end.diff(start, "days");
+      return days;
+    },
   },
   methods: {
-    logStartDate () {
-      console.log('Startdatum Prüfungszeitraum' + ' ' + this.startDate);
+    logStartDate() {
+      console.log("Startdatum Prüfungszeitraum" + " " + this.startDate);
     },
-    logEndDate () {
-      console.log('Enddatum Prüfungszeitraum' + ' ' + this.endDate);
+    logEndDate() {
+      console.log("Enddatum Prüfungszeitraum" + " " + this.endDate);
     },
     addDays() {
-      let result = new Date ();
-      result.setDate(result.getDate() +1);
-      return result
+      let result = new Date();
+      result.setDate(result.getDate() + 1);
+      return result;
     },
     getDates(startDate, stopDate) {
       let dateArray = new Array();
       let firstDate = moment(this.startDate);
       let lastDate = moment(this.endDate);
       while (firstDate <= lastDate) {
-        dateArray.push(new Date (firstDate));
-        firstDate = moment(firstDate).add (1, "days")
-        moment(this.firstDate).format('DD.MM.YYYY')
+        dateArray.push(new Date(firstDate));
+        firstDate = moment(firstDate).add(1, "days");
+        // moment(this.firstDate).format('DD.MM.YYYY')
       }
-      console.log(dateArray)
-      return dateArray
-    }
-  }
-}
+      const dateJson = {
+        Prüfungstage: dateArray,
+      };
+      console.log(JSON.stringify(dateJson));
+      // fix: Prüfungstage variable korrekt übergeben
+      this.period = dateArray;
+      return dateArray;
+    },
+    testtest() {
+      console.log(this.examOverview);
+    },
+  },
+};
 </script>
 
 <style scoped>
