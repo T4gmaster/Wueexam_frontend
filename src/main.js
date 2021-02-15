711d352122f30f0767d50b705b6a47bfde5ed8f5
Adrian König <adrian.koenig@hotmail.de>
2021-02-15 18:28:49 +0100

    Add login function

--- src/main.js
+++ src/main.js
@@ -21,6 +21,8 @@ import router from "./router/index";
 import PaperDashboard from "./plugins/paperDashboard";
 import "vue-notifyjs/themes/default.css";
 
+import Keycloak from 'keycloak-js';
+
 Vue.use(PaperDashboard);
 
 /* eslint-disable no-new */
@@ -28,3 +30,45 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+let initOptions = {
+  url: 'http://127.0.0.1:8080/auth', realm: 'WueExam', clientId: 'app-wueexam', onLoad: 'login-required'
+}
+
+let keycloak = Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) =>{
+    
+    if(!auth) {
+      window.location.reload();
+    } else {
+      Vue.$log.info("Authenticated");
+    }
+ 
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app');
+  
+
+    localStorage.setItem("vue-token", keycloak.token);
+    localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
+
+    setInterval(() =>{
+      keycloak.updateToken(70).success((refreshed)=>{
+        if (refreshed) {
+          Vue.$log.debug('Token refreshed'+ refreshed);
+        } else {
+          Vue.$log.warn('Token not refreshed, valid for '
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+        }
+      }).error(()=>{
+          Vue.$log.error('Failed to refresh token');
+      });
+
+
+    }, 60000)
+
+}).error(() =>{
+  Vue.$log.error("Authenticated Failed");
+});
