2bac09c4766ced0915a001a111958628795dc284
Adrian König <adrian.koenig@hotmail.de>
2021-02-24 23:04:49 +0100

    keycloak entfernen

--- src/main.js
+++ src/main.js
@@ -21,44 +21,9 @@ import router from "./router/index";
 import PaperDashboard from "./plugins/paperDashboard";
 import "vue-notifyjs/themes/default.css";
 
-import * as Keycloak from 'keycloak-js';
-
 Vue.use(PaperDashboard);
 
-let initOptions = {
-  url: 'http://132.187.226.24:8080/auth', realm: 'WueExam', clientId: 'app-wueexam', onLoad: 'login-required'
-}
-
-let keycloak = Keycloak(initOptions);
-Vue.prototype.$keycloak = keycloak;
-
-keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
-  if (!auth) {
-    window.location.reload();
-  } else {
-    console.log("Authenticated");
-
-    new Vue({
-      router,
-      render: h => h(App, { props: { keycloak: keycloak } }),
-    }).$mount("#app");
-  }
-
-
-//Token Refresh
-  setInterval(() => {
-    keycloak.updateToken(70).then((refreshed) => {
-      if (refreshed) {
-        console.log('Token refreshed' + refreshed);
-      } else {
-        console.log('Token not refreshed, valid for '
-          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-      }
-    }).catch(() => {
-      console.log('Failed to refresh token');
-    });
-  }, 6000)
-
-}).catch(() => {
-  console.log("Authenticated Failed");
-});
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
