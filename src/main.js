98b0e8684f049a47b4d3aa50643c6250b6b0c779
Adrian König <adrian.koenig@hotmail.de>
2021-03-30 21:28:56 +0200

    keycloak wieder eingefügt

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import PaperDashboard from "./plugins/paperDashboard";
 import "vue-notifyjs/themes/default.css";
 import VueApexCharts from "vue-apexcharts";
 
+import * as Keycloak from 'keycloak-js';
+
+
 
 Vue.config.productionTip = false
 Vue.prototype.$IPBE = 'http://132.187.226.24:5000'
@@ -18,7 +21,40 @@ Vue.component("apexchart", VueApexCharts);
 
 Vue.use(PaperDashboard);
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount("#app");
+let initOptions = {
+  url: 'http://132.187.226.24:8080/auth', realm: 'WueExam', clientId: 'app-wueexam', onLoad: 'login-required'
+}
+
+let keycloak = Keycloak(initOptions);
+Vue.prototype.$keycloak = keycloak;
+
+keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    console.log("Authenticated");
+
+    new Vue({
+      router,
+      render: h => h(App, { props: { keycloak: keycloak } }),
+    }).$mount("#app");
+  }
+
+
+//Token Refresh
+  setInterval(() => {
+    keycloak.updateToken(70).then((refreshed) => {
+      if (refreshed) {
+        console.log('Token refreshed' + refreshed);
+      } else {
+        console.log('Token not refreshed, valid for '
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+      }
+    }).catch(() => {
+      console.log('Failed to refresh token');
+    });
+  }, 6000)
+
+}).catch(() => {
+  console.log("Authenticated Failed");
+});
