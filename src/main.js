d21635cfaf297fd657d0c03228105858c1d899d8
Adrian König <adrian.koenig@hotmail.de>
2021-05-25 11:50:47 +0200

    update url

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ import VueApexCharts from "vue-apexcharts";
 import * as Keycloak from 'keycloak-js';
 
 Vue.config.productionTip = false
-Vue.prototype.$IPBE = 'localhost:5000'  //'http://132.187.226.24:5000'
-Vue.prototype.$IPOPTIMIZATION = 'localhost:5001'//'http://132.187.226.24:5001'
+Vue.prototype.$IPBE = 'http://localhost:5000'  //'http://132.187.226.24:5000'
+Vue.prototype.$IPOPTIMIZATION = 'http://localhost:5001'//'http://132.187.226.24:5001'
 Vue.prototype.$NAME = 'test'
 Vue.prototype.$PW = 'test1'
 
@@ -24,7 +24,7 @@ Vue.component("apexchart", VueApexCharts);
 Vue.use(PaperDashboard);
 
 let initOptions = {
-  url: 'localhost:8080/auth', realm: 'WueExam', clientId: 'app-wueexam', onLoad: 'login-required'
+  url: 'http://localhost:8080/auth', realm: 'WueExam', clientId: 'app-wueexam', onLoad: 'login-required'
 } //'http://132.187.226.24:8080/auth'
 
 let keycloak = Keycloak(initOptions);
